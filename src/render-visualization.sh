68b56973ab2e596e615450ac28dd2ee274137dc1
Bruce W. Herr II <bherr@iu.edu>
2026-01-14 11:31:02 -0500

    Add a retry mechanic for rendering the visualizations

--- src/render-visualization.sh
+++ src/render-visualization.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 source constants.sh
+set -o pipefail
 
 HTML=$1
 OUT="${HTML%.html}"
 
 echo "${OUT}.png"
-pageres $HTML 1440x788 --crop --delay=20 --overwrite --timeout=36000 --filename=$OUT
+
+for attempt in {1..3}; do
+  pageres $HTML 1440x788 --crop --delay=20 --overwrite --timeout=36000 --filename=$OUT && break
+  if [ $attempt -lt 3 ]; then
+    echo "Attempt $attempt failed, retrying..." >&2
+    sleep 1
+  fi
+done
